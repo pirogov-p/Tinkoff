f388e45024fed0532856de04942f02ebff55638d
Павел <pirogov-p@list.ru>
2023-02-27 19:20:21 +0300

    load

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 host = "127.0.0.1"
 user = "root"
-password = "Pirogov1391234!"
-db_name = "tinkoff"
+password = "Pirogov13912345!"
+db_name = "stocks"
 
 #token счёта 1
 token1 = 't.yI6LU8whBgcZnWH7ACOk1PhuLSTidns1XR8Ip10AcbvoqLWAhBUoAi7wF_AH0hL1Eiajbsy7ikCU6q54NvrE4w'
@@ -9,3 +9,6 @@ token1 = 't.yI6LU8whBgcZnWH7ACOk1PhuLSTidns1XR8Ip10AcbvoqLWAhBUoAi7wF_AH0hL1Eiaj
 token2 = 't.2egg_ANpbuo62jIzXl4lSVPCES1HXS_A8s4ib4vej7ypLG1SmkzmuOAMXrgPydedFf4IvBIs8SOB9WJHOPa3bA'
 #token счёта 3
 token3 = 't.7mMzu7tEhhws3_3EqMcJzM6iLftyVymyNaYXJtH5VyDWfrBumaXqvlEYoZKrNwsNSJTqV0M1cDsXpWPKeNwNgw'
+
+key = 'LT3BACWoNKMUfaTH6ul7laDm6qRx8RoMfLoGUGKkdKUADxucU8oR9D9ar6lBwDUS'
+secret = 'p23Pkvlr8kJWpgb9Xm3nMcW9lSydkFKglgLSAPdWx63fBgd85Dxczh6eax1hTNEf'
